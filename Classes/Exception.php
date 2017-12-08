328bfc520dc2ceea2bd1dfc2f0dda5be9cf140e1
Dominique Feyer <dfeyer@ttree.ch>
2017-12-08 13:31:50 +0100

    BUGFIX: Support Array as Suggestion input

---
+++ Classes/Exception.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Flowpack\SearchPlugin;
+
+/*
+ * This file is part of the Flowpack.SearchPlugin package.
+ *
+ * (c) Contributors of the Flowpack Team - flowpack.org
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+class Exception extends \Neos\Flow\Exception
+{
+
+}
