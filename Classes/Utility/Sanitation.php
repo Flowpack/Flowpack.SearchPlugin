0902359d81889aaa4fe8cacf60e22e536c946834
Daniel Lienert <daniel@lienert.cc>
2021-01-26 10:35:47 +0100

    TASK: Extract sanitation rule and use it in suggestions

---
+++ Classes/Utility/Sanitation.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Flowpack\SearchPlugin\Utility;
+
+/*
+ * This file is part of the Flowpack.SearchPlugin package.
+ *
+ * (c) Contributors of the Flowpack Team - flowpack.org
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+class Sanitation
+{
+
+    public static function sanitizeSearchInput(string $input): string
+    {
+        return str_replace(['=', '>', '<', '(', ')', '{', '}', '[', ']', '^', '"', '~', '*', '?', ':', '\\', '/'], ['', '', '', '(', '\)', '\{', '\}', '[', '\]', '\^', '\"', '\~', '\*', '\?', '\:', '\\\\', '\/'], $input);
+    }
+
+}
